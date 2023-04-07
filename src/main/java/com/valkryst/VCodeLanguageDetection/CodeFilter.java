88520188c643221c8fd02c83beeeee14172af39b
Valkryst <valkryst@valkryst.com>
2023-04-06 21:25:54 -0300

    Update `CodeFilter#filterSymbols` to not remove `.`

--- src/main/java/com/valkryst/VCodeLanguageDetection/CodeFilter.java
+++ src/main/java/com/valkryst/VCodeLanguageDetection/CodeFilter.java
@@ -53,7 +53,6 @@ public class CodeFilter {
                 .replace(';', ' ')
                 .replace(':', ' ')
                 .replace('\\', ' ')
-                .replace('/', ' ')
-                .replace('.', ' ');
+                .replace('/', ' ');
     }
 }
