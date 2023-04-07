6a58d8fe1de980ad6edb3cf81c8afa668a4ea156
Valkryst <valkryst@valkryst.com>
2023-04-06 21:06:59 -0300

    Update `CodeFilter#filterSymbols` to remove the `.` character

--- src/main/java/com/valkryst/VCodeLanguageDetection/CodeFilter.java
+++ src/main/java/com/valkryst/VCodeLanguageDetection/CodeFilter.java
@@ -38,6 +38,7 @@ public class CodeFilter {
                 .replace(';', ' ')
                 .replace(':', ' ')
                 .replace('\\', ' ')
-                .replace('/', ' ');
+                .replace('/', ' ')
+                .replace('.', ' ');
     }
 }
